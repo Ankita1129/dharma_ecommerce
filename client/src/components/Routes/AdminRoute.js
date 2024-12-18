8e0842d96829103dd585d81670388a3e8d18419e
Ankita1129 <2911ankitadev@gmail.com>
2024-12-18 20:32:37 +0530

    first commit

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -12,7 +12,7 @@ export default function PrivateRoute() {
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get("/api/v1/auth/admin-auth");
+      const res = await axios.get(`${process.env.BACKEND_URL}/api/v1/auth/admin-auth`);
       if (res.data.ok) {
         setOk(true);
       } else {
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("BACKEND_URL/api/v1/auth/login", {
+      const res = await axios.post(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
         email,
         password,
       });
